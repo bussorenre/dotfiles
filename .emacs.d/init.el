f2263acca53fd13e237daef14fe7781f3932ca7a
Ryo Matsumoto <ryo.mats302@gmail.com>
2014-12-10 10:23:21 +0900

    a

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,9 @@
-(prefer-coding-system 'utf-8)
+(set-language-environment 'Japanese)    ; 日本語環境
+(set-default-coding-systems 'utf-8-unix)    ; UTF-8 が基本
+(set-terminal-coding-system 'utf-8-unix)    ; emacs -nw も文字化けしない
+(setq default-file-name-coding-system 'utf-8)
+(setq default-process-coding-system '(utf-8 . utf-8))
+(prefer-coding-system 'utf-8-unix)
 
 (add-to-list 'load-path "~/.emacs.d/config/")
 
