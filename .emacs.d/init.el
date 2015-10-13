255e26079bec51d86a48ed0e02e17cefb38189d9
Ryo Matsumoto <ryo.mats302@gmail.com>
2015-10-13 15:52:06 +0900

    ac

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -61,4 +61,16 @@
 (global-undo-tree-mode t)
 (global-set-key (kbd "M-/") 'undo-tree-redo)
 
+;; ==================================================
+;; tramp / ssh remote file open
+;; ==================================================
+(require 'tramp)
+(setq tramp-default-method "ssh")
+(add-to-list 'tramp-default-proxies-alist
+	     '(nil "\\'root'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist
+	     '("localhost" nil nil))
+(add-to-list 'tramp-default-proxies-alist
+	     '((regexp-quote (system-name)) nil nil))
+
 
