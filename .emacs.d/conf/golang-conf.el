b5cb5e7bd7bb7fef259e75d58bbcbea1d7b8093b
Ryo Matsumoto <ryo.mats302@gmail.com>
2016-04-22 22:34:31 +0900

    golang-conf updated

--- .emacs.d/conf/golang-conf.el
+++ .emacs.d/conf/golang-conf.el
@@ -5,14 +5,22 @@
 ;; go get で入れたツールのパスを通す
 (add-to-list 'exec-path (expand-file-name "/Users/ryo/.go/bin"))
 
-
-
-;; go-mode の有効化
+;; 必要なパッケージのロード
 (require 'go-mode)
 (require 'company-go)
+;;(require 'flycheck-mode)
+
+;; 諸々の有効化、設定
 (add-hook 'go-mode-hook 'company-mode)
+(add-hook 'go-mode-hook 'flycheck-mode)
 (add-hook 'go-mode-hook (lambda()
 	       (add-hook 'before-save-hook' 'gofmt-before-save)
 	       (local-set-key (kbd "M-.") 'godef-jump)
 	       (set (make-local-variable 'company-backends) '(company-go))
-	       (company-mode)))
+	       (company-mode)
+	       (setq indent-tabs-mode nil)    ; タブを利用
+	       (setq c-basic-offset 4)        ; tabサイズを4にする
+	       (setq tab-width 4)))
+
+
+
