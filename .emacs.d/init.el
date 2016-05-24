90db0b5ff90e70536add5be92525239293373e92
Ryo Matsumoto <ryo.mats302@gmail.com>
2016-05-25 00:37:48 +0900

    fix company-tern mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -138,12 +138,19 @@
 ;; =================================================
 ;; company-mode company-tern
 ;; =================================================
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
 (setq company-tern-property-marker "")
 (defun company-tern-depth (candidate)
   "Return depth attribute for CANDIDATE. 'nil' entries are treated as 0."
   (let ((depth (get-text-property 0 'depth candidate)))
     (if (eq depth nil) 0 depth)))
-(add-hook 'js2-mode-hook 'tern-mode) ; 自分が使っているjs用メジャーモードに変える
+
+(add-hook 'js2-mode-hook
+	  (lambda ()
+	    (tern-mode t)))
+
 (add-to-list 'company-backends 'company-tern)
 
 
