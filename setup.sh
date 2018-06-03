2f74fce03acfc7c273ff1569865b6f283c885b11
Ryo Matsumoto <ryo.mats302@gmail.com>
2018-06-03 17:48:10 +0900

    improve setup mac workflow

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+brew_packages=array("hub" "rbenv" "emacs" "zsh" "tmux")
+
 # copy for HOME folder
 for file in `find $HOME/dotfiles -name '.*' | grep -v 'dotfiles/.git$' | perl -nle 'm!dotfiles/(.+)$! and print $1'`; do
     ln -s $HOME/dotfiles/$file $HOME/$file
@@ -8,15 +10,22 @@ done
 # delete gitignote 
 rm $HOME/.gitignore
 
+function setup_mac () {
+    # setup brew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    for p in "${packages[@]}"
+    do
+        brew install $p
+    done
+}
+
 # install spacemacs
 git clone git@github.com:bussorenre/spacemacs.git $HOME/.emacs.d
-
-# change shell to zsh
-chsh -s /bin/zsh
 
 
 case $OSTYPE in 
     darwin*)
+        setup_mac
         brew install reattach-to-user-namespace
         ;;
     linux*)
@@ -25,6 +34,9 @@ case $OSTYPE in
         ;; 
 
 esac
+
+# change shell to zsh
+chsh -s /bin/zsh
 
 # node でjavascript 保管等に必要なパッケージを入れる
 npm install -g tern js-beautify jshint
